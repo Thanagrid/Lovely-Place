e0d8ac1c73bd318cf408a7c9f90181fb52d4e840
Thanagrid Srikaew <vrzo20_@hotmail.co.th>
2024-02-06 07:34:24 +0700

    Update change username

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,4 +1,5 @@
 //import module
+const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();    // เรียกใช้ Router()
 const mysql = require('mysql2');    //import mysql2
@@ -34,6 +35,7 @@ router.post('/login/verify', (req,res)=>{
                 res.render('login',{message: 'Your Username or password is incorrect.'}); // render login.ejs ใหม่ โดยส่งข้อความไปบอก
             }else{                          // ถ้ามีข้อมูล user_name & user_password ตรงตามที่ผู้ใช้กรอก
                 res.cookie('user_id',results[0].user_id,{maxAge:3600000}); //เก็บ cookie user_id ไว้ ครึ่งชั่วโมง
+                res.cookie('user_name',results[0].user_name,{maxAge:3600000});
                 res.redirect('/');  //กลับไปที่ '/'
             };
         };
@@ -49,9 +51,9 @@ router.get('/register', (req,res)=>{
 // register verify รับ username & password จาก req registor.ejs
 router.post('/register/verify', (req,res)=>{
     //ตรวจสอบว่ามี user_name นี้อยู่แล้วรึเปล่า (เหมือนตรวจสอบว่ามีคนใช้ username นี้ไปรึยัง)
-    const sql_poolUsername = 'SELECT * FROM users WHERE user_name = ?;'
+    const sql_poolUser = 'SELECT * FROM users WHERE user_name = ?;'
     //ดึงข้อมูลจาก database เงื่อนไขตาม user_name ทีผู้ใช้ req มา
-    pool.query(sql_poolUsername, [req.body.username], (err, results, fields)=>{
+    pool.query(sql_poolUser, [req.body.username], (err, results, fields)=>{
         if(err){
             console.log(err);
             res.status(500);
@@ -69,6 +71,52 @@ router.post('/register/verify', (req,res)=>{
                 });
             }else{                      //ถ้ามีข้อมูลในตาราง (user_name ซ้ำกับที่มีอยู่แล้ว)
                 res.render('register',{message: 'Sorry, this username is already in use.'}); //render regis.ejs ใหม่ โดยส่งข้อความไปบอก
+            };
+        };
+    });
+});
+
+
+// Change Username page
+router.get('/change_username', (req,res)=>{
+    if(req.cookies.user_id){ //ตรวจสอบว่าเข้าระบบอยู่รึเปล่า
+         // ดึง username เดิมไปใส่เป็นค่าเริ่มต้นในฟอร์ม
+        res.render('change_username',{
+            username : req.cookies.user_name,
+            message: ''
+        });
+    }else{
+        res.redirect('/login');
+    }
+});
+
+//Change Username verify
+router.post('/change_username/verify', (req, res)=>{
+    const sql_poolUser = 'SELECT * FROM users WHERE user_name = ?;'
+    // ตรวจสอบว่าชื่อผู้ใช้ซ้ำรึเปล่า
+    pool.query(sql_poolUser, [req.body.username], (err, results, fields)=>{
+        if(err){
+            console.log(err);
+            res.status(500);
+        }else{
+            if(results.length == 0){  //ถ้า user_name ไม่ซ้ำ
+                // อัพเดต user_name ลงตาราง
+                const sql_updateUsername = 'UPDATE users SET user_name = ? WHERE user_id = ?;'
+                pool.query(sql_updateUsername,[req.body.username, req.cookies.user_id], (err, results, fields)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(500);
+                    }else{
+                         //เมื่ออัพเดตข้อมูลเสร็จแล้ว บันทึก cookie user_name ใหม่ แล้วส่งกลับไปยัง index
+                        res.cookie('user_name',req.body.username,{maxAge:3600000}); 
+                        res.redirect('/');
+                    }
+                });
+            }else{  //ถ้า user_name ซ้ำ
+                res.render('change_username',{
+                    username : req.cookies.user_name,
+                    message: 'Sorry, this username is already in use.'
+                });
             };
         };
     });
